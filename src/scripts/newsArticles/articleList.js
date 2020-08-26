3109c97b1d554737dfec522d231769c711998fc8
LukeZvada <zvadamusic@gmail.com>
2020-08-26 11:51:00 -0700

    render function moved / added articleList

--- src/scripts/newsArticles/articleList.js
+++ src/scripts/newsArticles/articleList.js
@@ -1,13 +1,22 @@
 import { useArticles, getArticles } from "./articleProvider.js";
 import { articleHTMLConverter } from "./articleHTMLConverter.js";
 
+
+
+export const articleList = () => { 
+    getArticles()
+    .then(() => { 
+        const allArticles = getArticles()
+        render(allArticles)
+    })
+}
+                            
+
 const render = () => { 
     const allArticlesHtml = articleArr.map (
         (currentArticle) => {
-           return articleHTMLConverter(currentArticle)  
-        }
-    ).join("")
-
-    contentTarget.innerHTML = allArticlesHtml
+            return articleHTMLConverter(currentArticle)
+            }  
+        ).join("")
+            contentTarget.innerHTML = allArticlesHtml
 }
-
