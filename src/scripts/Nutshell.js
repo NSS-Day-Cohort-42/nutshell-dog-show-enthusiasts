0c8af78d7e0f68f837bf81a8795e0fd228a74ce2
LukeZvada <zvadamusic@gmail.com>
2020-08-26 12:59:28 -0700

    create article button renders form as well as a save button / save button needs functionality.

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,6 +1,8 @@
 import { createArticleButton } from "./newsArticles/articleShowFormButton.js";
+import { articleForm } from "./newsArticles/articleForm.js";
 
 export const Nutshell = () => {
     // Render all your UI components here
     createArticleButton()
+    // articleForm()
 }
--- src/scripts/newsArticles/articleForm.js
+++ src/scripts/newsArticles/articleForm.js
@@ -4,8 +4,8 @@ const contentTarget = document.querySelector(".article--container")
 const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("click", clickEvent => { 
-    if (clickEvent.target.id === "createArticle") {
-        
+    if (clickEvent.target.id === "create__Article") {
+        render()
     }
 })
 
@@ -15,7 +15,11 @@ const render = () => {
         <input type="text" id ="article--synopsis" placeholder="Synopsis"/>
         <input type="text" id ="article--url" placeholder="Article URL"/>
         
-        
+        <button id="save__Article">Save Article</button> 
         <input type="hidden" id="articleId" name="articleId" value="">
     `
     } 
+
+export const articleForm = () => { 
+    render()
+}
