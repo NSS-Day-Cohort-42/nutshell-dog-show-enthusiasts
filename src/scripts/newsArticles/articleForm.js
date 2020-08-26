960a147b7d69cbc82137395d854953fef7b46d6a
LukeZvada <zvadamusic@gmail.com>
2020-08-26 13:49:05 -0700

    save article listening for event. saveArticle fetch needs debugging

--- src/scripts/newsArticles/articleForm.js
+++ src/scripts/newsArticles/articleForm.js
@@ -1,4 +1,4 @@
-
+import { saveArticle } from "./articleProvider.js";
 
 const contentTarget = document.querySelector(".article--container")
 const eventHub = document.querySelector(".container")
@@ -9,32 +9,30 @@ eventHub.addEventListener("click", clickEvent => {
     }
 })
 
-// eventHub.addEventListener("click", clickEvent => {
-//     if (clickEvent.target.id === "saveNote") {
+eventHub.addEventListener("click", clickEvent => {
+    console.log("Clicked saved Article")
+    if (clickEvent.target.id === "save__Article") {
 
-//         const noteTitle = document.querySelector("#note--title")
-//         const noteAuthor = document.querySelector("#note--author")
-//         const noteContent = document.querySelector("#note--content")
-//         const crimeId = document.querySelector("#criminalSelect")
-//         const [prompt, criminalId] = crimeId.value.split("--") //splitting at --. the prompt holds criminal
-//         // Make a new object representation of a note. //
+        const articleTitle = document.querySelector("#article--title")
+        const articleSynopsis = document.querySelector("#article--synopsis")
+        const articleURL = document.querySelector("#article--url")
 
-//         if (noteTitle.value !== "" && noteAuthor.value !== "" && noteContent.value !== "" && crimeId.value !== "0") {
-//             const editNoteId = document.querySelector("#noteId")
+            if(articleTitle !== "", articleSynopsis !== "", articleURL !== "") {
 
-//             if (editNoteId.value === "") {
-//                 const newNote = {
-//                     // Key/value pairs here
-//                     title: noteTitle.value,
-//                     author: noteAuthor.value,
-//                     content: noteContent.value,
-//                     timestamp: Date.now(),
-//                     criminalId: parseInt(criminalId) //parseInt turns a string into an integer 
-                    
-//                 }
-//                 // Change API state and application state
-//                 saveNote(newNote)
-//                 render()
+                const newArticle = {
+                    // Key/value pairs here
+                    title: articleTitle.value,
+                    synopsis: articleSynopsis.value,
+                    url: articleURL.value,
+                    timestamp: Date.now(),                    
+                }
+                saveArticle(newArticle)
+                render()
+            } else { 
+                window.alert("Fill in all the fields!!!")
+            }
+        }
+    })
 
 const render = () => {
     contentTarget.innerHTML = `
