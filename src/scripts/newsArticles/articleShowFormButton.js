bd291bd8a5027960e562b6d7a568dd2a8fb5ea18
LukeZvada <zvadamusic@gmail.com>
2020-08-26 11:56:28 -0700

    added new custom event for Create Article

---
+++ src/scripts/newsArticles/articleShowFormButton.js
@@ -0,0 +1,13 @@
+const contentTarget = document.querySelector(".article--container")
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "createArticle") { 
+        const createArticleEvent = new CustomEvent("createArticle")
+        eventHub.dispatchEvent(createArticleEvent)
+    }
+})
+
+export const createArticleButton = () => {
+    contentTarget.innerHTML = "<button id='create__Article'>Create Article</button>"
+}
