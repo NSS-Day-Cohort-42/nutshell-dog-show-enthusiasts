ec8d9471c0330cff9644d384021f499d04fabc82
LukeZvada <zvadamusic@gmail.com>
2020-08-26 11:33:04 -0700

    fetch call for articles created

--- src/scripts/newsArticles/articleProvider.js
+++ src/scripts/newsArticles/articleProvider.js
@@ -1,2 +1,14 @@
 let articles = [] 
 
+export const useArticles = () => {
+    articles.slice()
+}
+
+export const getArticles = () => {
+    return fetch('http://localhost:8088/database')
+        .then(response => response.json())
+        .then(parsedArticles => {
+            articles = parsedArticles
+        })
+
+}
