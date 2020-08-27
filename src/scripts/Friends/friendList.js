45d0cf011ebd02ffd438e4e7edd68fe65d3a6b67
justha <nganha42@gmail.com>
2020-08-27 15:54:08 -0500

    commit to merge

--- src/scripts/Friends/friendList.js
+++ src/scripts/Friends/friendList.js
@@ -5,21 +5,24 @@ import { friendHTMLConverter } from "./friendHTMLConverter.js";
 // const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".friends--list")
 
-// **NOTE** userId var needs to be dynamically defined based on username entered at sign in **NOTE**
-const signedInUserId = 1
 
-const render = (users, allRelationships) => {
-    // htmlRepresentations = users.map(user => {
-    //     const relationshipsForThisUser = allRelationships.filter(relationship => relationship.userId === signedInUserId)    //returns array of all friend objects for this userId
-    //     const friends = relationshipsForThisUser.map(friendObj => {                                                 //convert 
-    //         const matchingUserObj = users.find(user => user.userId === friendObj.friendUserId)
-    //         return matchingUserObj
-    //     })
-    //     return friendHTMLConverter(relationshipsForThisUser)
-    // }).join("")
+const render = (allUsers, allRelationships) => {
+    const currentUser = sessionStorage.getItem("activeUser")
     
-    let htmlRepresentations = ""
+    // const filteredUsers = allUsers.filter((user) => {
+    //     return currentUser === user.userId
+    // })
 
+
+    const htmlRepresentations = allUsers.map(user => {
+        const relationshipsForThisUser = allRelationships.filter(relationship => relationship.userId === currentUser)    //returns array of all friend objects for this userId
+        const friends = relationshipsForThisUser.map(friendObj => {                                                 //convert 
+            const matchingUserObj = allUsers.find(user => user.userId === friendObj.friendUserId)
+            return matchingUserObj
+        })
+        return friendHTMLConverter(relationshipsForThisUser)
+    }).join("")
+    
     contentTarget.innerHTML = `
         <h2>Friends</h2>
             <article id="friendList">
@@ -34,9 +37,9 @@ export const friendList = () => {
     getUsers()
     .then(getFriends)
     .then(() => {
-        const users = useUsers()
-        const friends = useFriends()
-        render(users, friends)
+        const allUsers = useUsers()
+        const allRelationships = useFriends()
+        render(allUsers, allRelationships)
     })
 }
 
