72d0aa7e879fe3c96b891d1a76dcc0dba6a3a06d
justha <nganha42@gmail.com>
2020-08-27 20:52:09 -0500

    friend list now re-renders when a friend is saved

--- src/scripts/Friends/friendList.js
+++ src/scripts/Friends/friendList.js
@@ -2,43 +2,10 @@ import { getUsers, useUsers } from "../Users/userProvider.js";
 import { getFriends, useFriends } from "../Friends/friendProvider.js";
 import { friendHTMLConverter } from "./friendHTMLConverter.js";
 
-// const eventHub = document.querySelector(".container")
+const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".friends--list")
 const currentUser = parseInt(sessionStorage.getItem("activeUser"))
 
-
-// const render = (allUsers, allRelationships) => {
-
-//     const relationshipsForThisUser = allRelationships.filter(relationship => relationship.userId === currentUser)
-//     console.log("console log 'relationshipsForThisUser' >>", relationshipsForThisUser)
-
-
-    
-
-
-    // const filteredUsers = allUsers.filter((user) => {
-    //     return currentUser === user.userId
-    // })
-
-
-    // const htmlRepresentations = allUsers.map(user => {
-    //     const relationshipsForThisUser = allRelationships.filter(relationship => relationship.userId === currentUser)    //returns array of all friend objects for this userId
-    //     const friends = relationshipsForThisUser.map(friendObj => {                                                 //convert 
-    //         const matchingUserObj = allUsers.find(user => user.userId === friendObj.friendUserId)
-    //         return matchingUserObj
-    //     })
-    //     console.log("console log 'relationshipsForThisUser' >>", relationshipsForThisUser)
-    //     return friendHTMLConverter(relationshipsForThisUser)
-    // }).join("")
-    
-
-    // contentTarget.innerHTML = `
-    //     <h2>Friends</h2>
-    //         <article id="friendList">
-    //         ${htmlRepresentations}
-    //         </article> 
-    // `
-// }
 
 
 const render = (allUsers, allRelationships) => {
@@ -49,7 +16,7 @@ const render = (allUsers, allRelationships) => {
         return matchingUserObj
     })
 
-    console.log("console log friends >>", friends)
+    // console.log("console log friends >>", friends)
 
     let htmlRepresentations = ""
     friends.forEach(friend => {
@@ -77,7 +44,6 @@ export const friendList = () => {
 
 
 
-// eventHub.addEventListener("friendStateChanged", () => {
-//     const newFriends = useFriends()
-//     renderNoteList(newFriends)
-// })
+eventHub.addEventListener("friendStateChanged", event => {
+    friendList()
+})
