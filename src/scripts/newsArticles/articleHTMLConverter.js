32dd821eb26ad72ffc3650b3fc17181b9c16f449
justha <nganha42@gmail.com>
2020-08-31 18:18:16 -0500

    articleHTMLConverter: added class names to style user and friend article cards differently

--- src/scripts/newsArticles/articleHTMLConverter.js
+++ src/scripts/newsArticles/articleHTMLConverter.js
@@ -25,15 +25,58 @@ eventHub.addEventListener("click", clickEvent => {
 })
 
 
-export const articleHTMLConverter = (articleObject) => {
-    return ` 
-        <section class="article--dashboard">
-            <div class="article__Title">Title: ${articleObject.title}</div> 
-            <div class="article__Synopsis">Synopsis: ${articleObject.synopsis}</div> 
-            <div class="article__url">url: ${articleObject.url}</div>
+// export const articleHTMLConverter = (articleObject) => {
+//     return ` 
+//         <section class="article--dashboard">
+//             <div class="article__Title">Title: ${articleObject.title}</div> 
+//             <div class="article__Synopsis">Synopsis: ${articleObject.synopsis}</div> 
+//             <div class="article__url">url: ${articleObject.url}</div>
+//             <div class="article__date">Saved: ${new Date(articleObject.timestamp).toLocaleDateString('en-US')}</div>
+
             
-            <button class="deleteArticle" id='delete--${ articleObject.id }'>Delete</button>
-            <button class="editArticle" id='edit--${ articleObject.id }'>Edit</button>
-        </section>
-    `
+//             <button class="deleteArticle" id='delete--${ articleObject.id }'>Delete</button>
+//             <button class="editArticle" id='edit--${ articleObject.id }'>Edit</button>
+//         </section>
+//     `
+// }
+
+
+
+// friend articles: <i> tags (italic font), Cornsilk background color, no edit/delete buttons
+export const articleHTMLConverter = (articleObject) => {
+    const activeUserId = parseInt(sessionStorage.getItem("activeUser"))
+    console.log("activeUserId >>",activeUserId)
+    if (articleObject.userId === activeUserId) {
+        return ` 
+            <section class="article--dashboard">
+                <article class="article__cardUser">
+                    <div class="article__Title">Title: ${articleObject.title}</div> 
+                    <div class="article__Synopsis">Synopsis: ${articleObject.synopsis}</div> 
+                    <div class="article__url">url: ${articleObject.url}</div>
+                    <div class="article__date">Saved: ${new Date(articleObject.timestamp).toLocaleDateString('en-US')}</div>
+
+                    
+                    <button class="deleteArticle" id='delete--${ articleObject.id }'>Delete</button>
+                    <button class="editArticle" id='edit--${ articleObject.id }'>Edit</button>
+                </article>
+            </section>
+        `
+    }
+    else {
+        return ` 
+            <section class="article--dashboard">
+                <i>
+                <article class="article__cardFriend" style="background-color:Cornsilk;">
+                    <div class="article__Title">Title: ${articleObject.title}</div> 
+                    <div class="article__Synopsis">Synopsis: ${articleObject.synopsis}</div> 
+                    <div class="article__url">url: ${articleObject.url}</div>
+                    <div class="article__date">Saved: ${new Date(articleObject.timestamp).toLocaleDateString('en-US')}</div>
+                </article>
+                </i>
+        
+                
+            </section>
+        `
+    }
+
 }
