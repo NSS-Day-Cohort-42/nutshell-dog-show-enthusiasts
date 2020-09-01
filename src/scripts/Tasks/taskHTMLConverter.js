c119e99e6ce4ff6ee473125a8a525accc02ccd74
LukeZvada <zvadamusic@gmail.com>
2020-08-31 19:33:26 -0700

    article fixed. events styled. weather styled

--- src/scripts/Tasks/taskHTMLConverter.js
+++ src/scripts/Tasks/taskHTMLConverter.js
@@ -8,13 +8,13 @@ const eventHub = document.querySelector(".container")
 export const taskHTMLCon = (task) => {
     return `
         <div class="card__task" id="taskCards">
-            <input type="checkbox" id="taskCompletedChecked--${task.id}" name="taskBox">
-            <label for="taskBox">Completed</label>
+            <input class="taskBoxCompleted" type="checkbox" id="taskCompletedChecked--${task.id}" name="taskBox">
+            <label class="taskBoxComplete" for="taskBox">Completed</label>
             <div class="task__title">Task Name :${task.taskTitle}</div>
             <div class="task__content">Description :${task.description}</div>
             <div class="task__completionDate">Expected Completion: ${task.taskDate}</div>
-            <button id="deleteTask--${task.id}">Delete</button>
-            <button id="editTask--${task.id}">Edit</button>
+            <button class="deleteTaskButton" id="deleteTask--${task.id}">Delete</button>
+            <button class="editTaskButton" id="editTask--${task.id}">Edit</button>
         </div>
     `
 }
